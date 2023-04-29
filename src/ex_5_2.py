f60cb9dfa747f536b770c23bb8c9b3df887c9cff
Syed Faisal Ahmed <slnu2@cbu.edu>
2023-04-29 10:10:47 +0530

    checking

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     # Use these predefined input / output files
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
-    OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
+    OUTFILE = root_dir / "data" / "ex_5_2-processed.csv"
 
     # Complete the data processing steps using numpy here.
     data = np.loadtxt(INFILE, delimiter=",")
